250e2014ffaa1deb6cea485d80aae47a6b52f8d5
moksedulhaque01 <moksedul13psb@gmail.com>
2025-08-31 19:17:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+	/* alert("Hello world");
+
+	$('#sbtn').click(function(){
+		alert("Hello SSB");
+	});
+
+	$('button').on('click',function(){
+		alert("Hello SSB");
+	}); */
+
+	$('#show').on('click',function(){
+		$('h3').show();
+	});
+
+	$('#hide').on('click',function(){
+		$('h3').hide();
+	});
+
+	$('#toggle').on('click',function(){
+		$('h3').toggle(2000);
+	});
+
+	$('.ques').on('click',function(){
+		$('.answ').slideToggle(300);
+	});
+
+	$( "#draggable" ).draggable();
+
+	$( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+    $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+});
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+
+     var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+
+    $( "#tabs" ).tabs();
+
+        $( document ).tooltip();
+  } );
+
+
